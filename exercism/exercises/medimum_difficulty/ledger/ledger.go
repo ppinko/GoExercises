1d359c9662c62309fd93a045c2df9b6e740c5136
ppinko <p.pinkowicz@gmail.com>
2022-06-19 18:52:51 +0200

    REFACTORING - Remove parallel computing

--- exercism/exercises/medimum_difficulty/ledger/ledger.go
+++ exercism/exercises/medimum_difficulty/ledger/ledger.go
@@ -51,140 +51,118 @@ func FormatLedger(currency string, locale string, entries []Entry) (string, erro
 	}
 	s := w1 + strings.Repeat(" ", 10-len(w1)) + " | " + w2 + strings.Repeat(" ", 25-len(w2)) + w3
 
-	type intermediate struct {
-		i int
-		s string
-		e error
-	}
-
-	co := make(chan intermediate)
-	for i, et := range entriesCopy {
-		go func(i int, entry Entry) {
-			if len(entry.Date) != 10 {
-				co <- intermediate{e: errors.New("")}
-			}
-			d1, d2, d3, d4, d5 := entry.Date[0:4], entry.Date[4], entry.Date[5:7], entry.Date[7], entry.Date[8:]
-			if (d2 != '-') || (d4 != '-') {
-				co <- intermediate{e: errors.New("")}
-			}
-
-			de := entry.Description
-			if len(de) > 25 {
-				de = de[:22] + "..."
-			} else {
-				de = de + strings.Repeat(" ", 25-len(de))
-			}
-
-			var d string
-			if locale == "nl-NL" {
-				d = d5 + "-" + d3 + "-" + d1
-			} else if locale == "en-US" {
-				d = d3 + "/" + d5 + "/" + d1
-			}
-
-			negative := false
-			cents := entry.Change
-			if cents < 0 {
-				cents = cents * -1
-				negative = true
-			}
-
-			var a string
-			if locale == "nl-NL" {
-				switch currency {
-				case "EUR":
-					{
-						a += "€ "
-					}
-				default:
-					{
-						a += "$ "
-					}
-				}
-				centsStr := strconv.Itoa(cents)
-				switch len(centsStr) {
-				case 1:
-					centsStr = "00" + centsStr
-				case 2:
-					centsStr = "0" + centsStr
-				}
-				rest := centsStr[:len(centsStr)-2]
-
-				var parts []string
-				for len(rest) > 3 {
-					parts = append(parts, rest[len(rest)-3:])
-					rest = rest[:len(rest)-3]
-				}
-				if len(rest) > 0 {
-					parts = append(parts, rest)
-				}
-				for i := len(parts) - 1; i >= 0; i-- {
-					a += parts[i] + "."
-				}
-				a = a[:len(a)-1]
-				a += ","
-				a += centsStr[len(centsStr)-2:]
-				if negative {
-					a += "-"
-				} else {
-					a += " "
-				}
-			} else {
-				if negative {
-					a += "("
-				}
-				if currency == "EUR" {
-					a += "€"
-				} else {
-					a += "$"
-				}
-
-				centsStr := strconv.Itoa(cents)
-				switch len(centsStr) {
-				case 1:
-					centsStr = "00" + centsStr
-				case 2:
-					centsStr = "0" + centsStr
-				}
-				rest := centsStr[:len(centsStr)-2]
-				var parts []string
-				for len(rest) > 3 {
-					parts = append(parts, rest[len(rest)-3:])
-					rest = rest[:len(rest)-3]
-				}
-				if len(rest) > 0 {
-					parts = append(parts, rest)
-				}
-				for i := len(parts) - 1; i >= 0; i-- {
-					a += parts[i] + ","
-				}
-				a = a[:len(a)-1]
-				a += "."
-				a += centsStr[len(centsStr)-2:]
-				if negative {
-					a += ")"
-				} else {
-					a += " "
-				}
-			}
-
-			al := utf8.RuneCountInString(a)
-
-			co <- intermediate{i, d + strings.Repeat(" ", 10-len(d)) + " | " + de + " | " + strings.Repeat(" ", 13-al) + a + "\n", nil}
-		}(i, et)
-	}
-
-	ss := make([]string, len(entriesCopy))
-	for range entriesCopy {
-		v := <-co
-		if v.e != nil {
-			return "", v.e
+	for _, et := range entriesCopy {
+		if len(et.Date) != 10 {
+			return "", errors.New("invalid input")
 		}
-		ss[v.i] = v.s
-	}
+		d1, d2, d3, d4, d5 := et.Date[0:4], et.Date[4], et.Date[5:7], et.Date[7], et.Date[8:]
+		if (d2 != '-') || (d4 != '-') {
+			return "", errors.New("invalid input")
+		}
 
-	for i := 0; i < len(entriesCopy); i++ {
-		s += ss[i]
+		de := et.Description
+		if len(de) > 25 {
+			de = de[:22] + "..."
+		} else {
+			de = de + strings.Repeat(" ", 25-len(de))
+		}
+
+		var d string
+		if locale == "nl-NL" {
+			d = d5 + "-" + d3 + "-" + d1
+		} else if locale == "en-US" {
+			d = d3 + "/" + d5 + "/" + d1
+		}
+
+		negative := false
+		cents := et.Change
+		if cents < 0 {
+			cents = cents * -1
+			negative = true
+		}
+
+		var a string
+		if locale == "nl-NL" {
+			switch currency {
+			case "EUR":
+				{
+					a += "€ "
+				}
+			default:
+				{
+					a += "$ "
+				}
+			}
+			centsStr := strconv.Itoa(cents)
+			switch len(centsStr) {
+			case 1:
+				centsStr = "00" + centsStr
+			case 2:
+				centsStr = "0" + centsStr
+			}
+			rest := centsStr[:len(centsStr)-2]
+
+			var parts []string
+			for len(rest) > 3 {
+				parts = append(parts, rest[len(rest)-3:])
+				rest = rest[:len(rest)-3]
+			}
+			if len(rest) > 0 {
+				parts = append(parts, rest)
+			}
+			for i := len(parts) - 1; i >= 0; i-- {
+				a += parts[i] + "."
+			}
+			a = a[:len(a)-1]
+			a += ","
+			a += centsStr[len(centsStr)-2:]
+			if negative {
+				a += "-"
+			} else {
+				a += " "
+			}
+		} else {
+			if negative {
+				a += "("
+			}
+			if currency == "EUR" {
+				a += "€"
+			} else {
+				a += "$"
+			}
+
+			centsStr := strconv.Itoa(cents)
+			switch len(centsStr) {
+			case 1:
+				centsStr = "00" + centsStr
+			case 2:
+				centsStr = "0" + centsStr
+			}
+			rest := centsStr[:len(centsStr)-2]
+			var parts []string
+			for len(rest) > 3 {
+				parts = append(parts, rest[len(rest)-3:])
+				rest = rest[:len(rest)-3]
+			}
+			if len(rest) > 0 {
+				parts = append(parts, rest)
+			}
+			for i := len(parts) - 1; i >= 0; i-- {
+				a += parts[i] + ","
+			}
+			a = a[:len(a)-1]
+			a += "."
+			a += centsStr[len(centsStr)-2:]
+			if negative {
+				a += ")"
+			} else {
+				a += " "
+			}
+		}
+
+		al := utf8.RuneCountInString(a)
+
+		s += d + strings.Repeat(" ", 10-len(d)) + " | " + de + " | " + strings.Repeat(" ", 13-al) + a + "\n"
 	}
 
 	return s, nil
